4cbe4d67e6f857e96c7e058a71966d5fb2999b71
Petr Savichev (proton) <psavichev@gmail.com>
2021-05-08 22:05:42 -0500

    ruby - merge sort - init buffer once

--- ruby/sort/merge_sort.rb
+++ ruby/sort/merge_sort.rb
@@ -1,6 +1,7 @@
-def merge_sort(array, start: nil, finish: nil)
+def merge_sort(array, start: nil, finish: nil, buffer: nil)
   start  ||= 0
   finish ||= array.size - 1
+  buffer ||= Array.new(array.size)
 
   return if start == finish
 
@@ -9,41 +10,48 @@ def merge_sort(array, start: nil, finish: nil)
   merge_sort(array, start: start,   finish: mid)
   merge_sort(array, start: mid + 1, finish: finish)
 
-  # join!
-  new_array = []
   i = 0
   j = 0
+  b = 0
   loop do
     if i == mid + 1 - start
-      new_array += array[(j + mid + 1)..finish]
+      ((j + mid + 1)..finish).each do |k|
+        buffer[b] = array[k]
+        b += 1
+      end
       break
     end
     if j == finish - mid
-      new_array += array[(i + start)..mid]
+      ((i + start)..mid).each do |k|
+        buffer[b] = array[k]
+        b += 1
+      end
       break
     end
     x = array[i + start]
     y = array[j + mid + 1]
     if x < y
-      new_array << x
+      buffer[b] = x
       i += 1
+      b += 1
     else
-      new_array << y
+      buffer[b] = y
       j += 1
+      b += 1
     end
   end
-  new_array.each_with_index do |x, i|
-    array[start + i] = x
+  (0...b).each do |k|
+    array[start + k] = buffer[k]
   end
 
   array
 end
 
-array = 100.times.map { rand(100) }
-array1 = array.sort
-array2 = merge_sort(array)
+# array = 100.times.map { rand(100) }
+# array1 = array.sort
+# array2 = merge_sort(array)
 
-p array1
-p array2
+# p array1
+# p array2
 
-fail unless array1 == array2
+# fail unless array1 == array2
