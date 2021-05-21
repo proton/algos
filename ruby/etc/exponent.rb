0e523c50a22c400ee3b5fcf78b10f9c398fd462b
Petr Savichev (proton) <psavichev@gmail.com>
2021-05-20 23:06:21 -0500

    exponent - ruby

---
+++ ruby/etc/exponent.rb
@@ -0,0 +1,21 @@
+def exponent(base, power)
+  return 1 if power == 0
+
+  result = 1
+
+  factor = base
+
+  while power > 0
+    result *= factor if power % 2 == 1
+    factor *= factor
+    power /= 2
+  end
+
+  result
+end
+
+# fail unless exponent(2,  3) ==  2**3
+# fail unless exponent(11, 5) == 11**5
+# fail unless exponent(17, 7) == 17**7
+# fail unless exponent(17, 8) == 17**8
+# fail unless exponent(17, 9) == 17**9
