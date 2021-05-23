d34d6b59ef5b3ad0fe2f9eddfc8b016eb1c9dafe
Petr Savichev (proton) <psavichev@gmail.com>
2021-05-22 22:44:13 -0500

    is prime - ruby

---
+++ ruby/etc/is_prime.rb
@@ -0,0 +1,26 @@
+def is_prime?(num)
+  return false if num < 2
+
+  # Fermat's little theorem
+  10.times do
+    i = rand(1...num)
+    return false if i ** (num - 1) % num != 1
+  end
+
+  # A head-on solution
+  (2..(num ** 0.5)).each do |n|
+    return false if num % n == 0
+  end
+
+  true
+end
+
+fail unless is_prime?(0)  == false
+fail unless is_prime?(1)  == false
+fail unless is_prime?(5)  == true
+fail unless is_prime?(10) == false
+fail unless is_prime?(11) == true
+fail unless is_prime?(16) == false
+fail unless is_prime?(17) == true
+fail unless is_prime?(31) == true
+fail unless is_prime?(32) == false
