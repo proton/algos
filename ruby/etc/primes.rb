945a98c1f0b34e615f65b8cd06ea4ccfab20ab23
Petr Savichev (proton) <psavichev@gmail.com>
2021-05-28 00:22:31 -0500

    primes - ruby

---
+++ ruby/etc/primes.rb
@@ -0,0 +1,33 @@
+def primes(max)
+  return [] if max <  2
+
+  l = [true] * (max + 1)
+
+  arr = [2]
+
+  p = 3
+
+  while p <= max
+    if l[p]
+      arr << p
+
+      factor = 2
+      p_mult = p * factor
+
+      while p_mult <= max
+        l[p_mult] = false
+        factor += 1
+        p_mult = p * factor
+      end
+    end
+
+    p += 2
+  end
+
+  arr
+end
+
+# fail unless primes(0)   == []
+# fail unless primes(2)   == [2]
+# fail unless primes(10)  == [2, 3, 5, 7]
+# fail unless primes(100) == [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97]
