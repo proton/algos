b8263b0ac382f6bf49da08c5d917be2cb567e577
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-07 16:24:12 -0500

    ruby - quich sort with dijkstra 3-way partitioning

---
+++ ruby/sort/quick_sort_dijkstra.rb
@@ -0,0 +1,40 @@
+def quick_sort_dijkstra(array, start: nil, finish: nil)
+  start  ||= 0
+  finish ||= array.size - 1
+
+  return if start >= finish
+
+  left    = start
+  right   = finish
+  i       = start
+
+  divider = array.values_at(start, start + (finish - start) / 2, finish).sort[1]
+
+  while i <= right
+    case array[i] <=> divider
+    when -1
+      array[i], array[left] = array[left], array[i]
+      i    += 1
+      left += 1
+    when 0
+      i += 1
+    when 1
+      array[i], array[right] = array[right], array[i]
+      right -= 1
+    end
+  end
+
+  quick_sort_dijkstra(array, start: start,     finish: left - 1)
+  quick_sort_dijkstra(array, start: right + 1, finish: finish  )
+
+  array
+end
+
+array = 100.times.map { rand(100) }
+array1 = array.sort
+array2 = quick_sort_dijkstra(array)
+
+p array1
+p array2
+
+fail unless array1 == array2
