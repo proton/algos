478f0a51897e27b551a8abeefd52a6ce2a2c9ed0
Petr Savichev (proton) <psavichev@gmail.com>
2021-06-14 20:37:24 -0500

    Add javascript Heap

---
+++ javascript/structure/Heap.js
@@ -0,0 +1,90 @@
+class Heap {
+  constructor(values = []) {
+    this.data = values
+
+    for (let i = Math.floor(this.data.length / 2) - 1; i >= 0; --i) {
+      this.#heapify(i)
+    }
+  }
+
+  push(value) {
+    this.data.push(value)
+
+    let i = this.data.length - 1
+    let parentI = this.#parentIndex(i)
+
+    while (i > 0 && this.data[parentI] < this.data[i]) {
+      this.#swap(i, parentI)
+      i = parentI
+      parentI = this.#parentIndex(i)
+    }
+  }
+
+  max() {
+    return this.data[0]
+  }
+
+  pop() {
+    const value = this.max()
+    const last  = this.data.pop()
+    if (this.data.length > 0) {
+      this.data[0] = last
+      this.#heapify(0)
+    }
+    return value
+  }
+
+  length() {
+    return this.data.length
+  }
+
+  #parentIndex(i) {
+    return Math.floor((i - 1) / 2)
+  }
+
+  #swap(i, j) {
+    [this.data[i], this.data[j]] = [this.data[j], this.data[i]]
+  }
+
+  #heapify(i) {
+    while (true) {
+      const leftChild  = 2 * i + 1
+      const rightChild = 2 * i + 2
+      let largestChild = i
+
+      if (leftChild < this.length() && this.data[leftChild] > this.data[largestChild]) {
+        largestChild = leftChild
+      }
+      if (rightChild < this.length() && this.data[rightChild] > this.data[largestChild]) {
+        largestChild = rightChild
+      }
+
+      if (largestChild === i) {
+        break
+      }
+
+      this.#swap(i, largestChild)
+      i = largestChild
+    }
+  }
+}
+
+// const arr1 = [8, 9, 5, 2, 1, 6, 7, 4, 10, 3]
+// const arr2 = [1, 6, 8, 9, 3, 5, 2, 10, 4, 7]
+// const arr3 = [6, 3, 8, 5, 9, 10, 4, 2, 7, 1]
+
+// const heap1 = new Heap(arr1)
+// const heap2 = new Heap(arr2)
+// const heap3 = new Heap()
+// arr3.forEach(x => heap3.push(x))
+
+// const heaps = [heap1, heap2, heap3]
+// console.log(heap1.data)
+// console.log(heap3.data)
+
+// heaps.forEach(heap => console.assert(heap.max() === 10))
+
+// while (heaps[0].length() > 0) {
+//   heaps.forEach(heap => console.assert(heap.max() === heaps[0].length()))
+//   console.assert(new Set(heaps.map(heap => heap.pop())).size === 1)
+// }
